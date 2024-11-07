24a89b84d3c24ea6f6be38af6fdfc919ddf4da01
prashanth <prashanthh036@gmail.com>
2024-11-07 20:09:35 +0530

    commit

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: '',
+  base: "/E-commerce-ToyStoreApp-/",
   plugins: [react()],
-  base: "/E-commerce-ToyStoreApp-/"
 
 })
